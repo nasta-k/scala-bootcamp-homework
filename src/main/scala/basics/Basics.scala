08b13a51477e0ff919abd4b73b9bd3dda5043218
nasta-k <kashko.nastia@yandex.ru>
2021-02-01 21:23:21 +0300

    commit -m "fixed zero case"

--- src/main/scala/basics/Basics.scala
+++ src/main/scala/basics/Basics.scala
@@ -3,9 +3,9 @@ package basics
 object Basics {
   @scala.annotation.tailrec
   def gcd(a: Int, b: Int): Int = {
-    if (a == 0) return b
+    if (a == 0) b else
     gcd(b % a, a)
   }
 
-  def lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
+  def lcm(a: Int, b: Int): Int = if (gcd(a,b)!=0) a * b / gcd(a, b) else 0
 }
