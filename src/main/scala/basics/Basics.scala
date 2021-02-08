d448269d30b86614342c67b43de507e35a9167cf
nasta-k <kashko.nastia@yandex.ru>
2021-02-08 23:44:46 +0300

    fixed basics

--- src/main/scala/basics/Basics.scala
+++ src/main/scala/basics/Basics.scala
@@ -4,8 +4,13 @@ object Basics {
   @scala.annotation.tailrec
   def gcd(a: Int, b: Int): Int = {
     if (a == 0) b else
-    gcd(b % a, a)
+      gcd(b % a, a)
   }
 
-  def lcm(a: Int, b: Int): Int = if (gcd(a,b)!=0) a * b / gcd(a, b) else 0
+  def lcm(a: Int, b: Int): Int = if (gcd(a, b) != 0) a * b / gcd(a, b) else 0
+
+  def main(args: Array[String]): Unit = {
+    println(lcm(34, 6))
+    println(gcd(129, 3))
+  }
 }
