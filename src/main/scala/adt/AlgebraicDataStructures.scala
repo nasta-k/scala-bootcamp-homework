6b37e3828904a2271fefc291839fa9f867f6ceb1
nasta-k <kashko.nastia@yandex.ru>
2021-02-22 21:58:03 +0300

    adt fixed

--- src/main/scala/adt/AlgebraicDataStructures.scala
+++ src/main/scala/adt/AlgebraicDataStructures.scala
@@ -4,29 +4,68 @@ import scala.collection.SortedSet
 
 object AlgebraicDataStructures {
 
-  sealed abstract class Suit
-
-  object Suit {
-    final case class Hearts(value: Char) extends Suit
-    final case class Diamonds(value: Char) extends Suit
-    final case class Clubs(value: Char) extends Suit
-    final case class Spades(value: Char) extends Suit
-    def create(value: Char): Option[Suit] = value.toLower match {
-          case 'h' =>Some(Hearts(value))
-          case 'd' => Some(Diamonds(value))
-          case 'c' => Some(Clubs(value))
-          case 's' => Some(Spades(value))
-          case _ => None
-        }
+  sealed abstract class Suit {
+    def value: Char
   }
 
-  final case class Rank(value: Char) extends AnyVal
+  object Suit {
+    case object Hearts extends Suit {
+      val value: Char = 'h'
+    }
+    case object Diamonds extends Suit {
+      val value: Char = 'd'
+    }
+    case object Clubs extends Suit {
+      val value: Char = 'c'
+    }
+    case object Spades extends Suit {
+      val value: Char = 's'
+    }
+  }
+
+  sealed abstract class Rank {
+    def value: Char
+  }
 
   object Rank {
-    def create(value: Char): Option[Rank] = value match {
-      case v if (2 to 9).map(_.toString.head) contains v => Some(Rank(value))
-      case 'T' | 'J' | 'Q' | 'K' | 'A' => Some(Rank(value))
-      case _ => None
+    case object Two extends Rank {
+      override def value: Char = '2'
+    }
+    case object Three extends Rank {
+      override def value: Char = '3'
+    }
+    case object Four extends Rank {
+      override def value: Char = '4'
+    }
+    case object Five extends Rank {
+      override def value: Char = '5'
+    }
+    case object Six extends Rank {
+      override def value: Char = '6'
+    }
+    case object Seven extends Rank {
+      override def value: Char = '7'
+    }
+    case object Eight extends Rank {
+      override def value: Char = '8'
+    }
+    case object Nine extends Rank {
+      override def value: Char = '9'
+    }
+    case object Ten extends Rank {
+      override def value: Char = 'T'
+    }
+    case object Jack extends Rank {
+      override def value: Char = 'J'
+    }
+    case object Queen extends Rank {
+      override def value: Char = 'Q'
+    }
+    case object King extends Rank {
+      override def value: Char = 'K'
+    }
+    case object Ace extends Rank {
+      override def value: Char = 'A'
     }
   }
 
@@ -36,39 +75,45 @@ object AlgebraicDataStructures {
 
   object Hand {
     final case class OmahaHand(cards: Set[Card]) extends Hand
-    final case class TexasHand(cards: Set[Card]) extends Hand
-    def create(cards: Set[Card]): Option[Hand] =
-      cards.size match {
-        case 2 => Some(TexasHand(cards))
-        case 4 => Some(OmahaHand(cards))
-        case _ => None
+    object OmahaHand{
+      def create(cards: Set[Card]): Option[Hand] = {
+        if (cards.size == 4) Some(OmahaHand(cards))
+        else None
       }
-  }
-
-  final case class Board(cards: Set[Card]) extends AnyVal
-
-  object Board {
-    def create(cards: Set[Card]): Option[Board] = {
-      if (cards.toList.length == 5) Some(Board(cards))
-      else None
+    }
+    final case class TexasHand(cards: Set[Card]) extends Hand
+    object TexasHand{
+      def create(cards: Set[Card]): Option[Hand] = {
+        if (cards.size == 2) Some(OmahaHand(cards))
+        else None
+      }
     }
   }
 
-  sealed trait PokerCombination
+    final case class Board(cards: Set[Card]) extends AnyVal
 
-  object PokerCombination {
-    final case class HighCard() extends PokerCombination
-    final case class Pair() extends PokerCombination
-    final case class ThreeOfAKind() extends PokerCombination
-    final case class Straight() extends PokerCombination
-    final case class Flush() extends PokerCombination
-    final case class FullHouse() extends PokerCombination
-    final case class FourOfAKind() extends PokerCombination
-    final case class StraightFlush() extends PokerCombination
+    object Board {
+      def create(cards: Set[Card]): Option[Board] = {
+        if (cards.size == 5) Some(Board(cards))
+        else None
+      }
+    }
+
+    sealed trait PokerCombination
+
+    object PokerCombination {
+      case object HighCard extends PokerCombination
+      case object Pair extends PokerCombination
+      case object ThreeOfAKind extends PokerCombination
+      case object Straight extends PokerCombination
+      case object Flush extends PokerCombination
+      case object FullHouse extends PokerCombination
+      case object FourOfAKind extends PokerCombination
+      case object StraightFlush extends PokerCombination
+    }
+
+    final case class TestCase(board: Board, hands: Set[Hand])
+
+    final case class TestResult(board: Board, hands: SortedSet[Hand])
+
   }
-
-  final case class TestCase(board: Board, hands: Set[Hand])
-
-  final case class TestResult(board: Board, hands: SortedSet[Hand])
-
-}
