d2464556551039648f8dd4a427c0cafe7b5fc8c0
nasta-k <kashko.nastia@yandex.ru>
2021-02-28 17:57:06 +0300

    sbt fixed

--- bulky-source-plugin/src/main/scala/plugins/BulkySourcePlugin.scala
+++ bulky-source-plugin/src/main/scala/plugins/BulkySourcePlugin.scala
@@ -6,23 +6,31 @@ import sbt.Keys._
 object BulkySourcePlugin extends AutoPlugin {
   override def trigger = allRequirements
 
-  val bulkySources = taskKey[Seq[(Int, File)]]("Sequence of files longer than 100 lines in descending order.")
+  object autoImport {
+    val bulkySources = taskKey[Seq[(Int, File)]]("Sequence of files longer than 100 lines in descending order.")
+    val bulkyThresholdInLines = settingKey[Int]("Threshold for lines")
+  }
 
-  val bulkyThresholdInLines = settingKey[Int]("Threshold for lines")
+  import autoImport._
 
-  override lazy val globalSettings: Seq[Setting[_]] = Seq(
-    bulkyThresholdInLines := 100
-  )
+  override val projectSettings: Seq[Setting[_]] =
+    inConfig(Compile)(bulkySourceSettings) ++ inConfig(Test)(bulkySourceSettings) ++ {
+      bulkyThresholdInLines := 100
+    }
 
-  def getBulkyLinesFiles(files: Seq[File], threshold: Int): Seq[(Int, File)] = {
+  def bulkySourceSettings: Seq[Setting[_]] = Seq(
+    bulkySources := getBulkyLinesFiles(sources.value))
+
+  def getBulkyLinesFiles(files: Seq[File]): Seq[(Int, File)] = {
     files.map { file => (sbt.IO.readLines(file).length, file) }
-      .filter { case (len, _) => len > threshold }
+      .filter { case (len, _) => len > bulkyThresholdInLines.value }
       .sortBy { case (len, _) => len }
       .reverse
   }
 
-  override val projectSettings: Seq[Setting[_]] = {
-    bulkySources := getBulkyLinesFiles((sources in Compile).value, bulkyThresholdInLines.value)
-    (Test / bulkySources) := getBulkyLinesFiles((sources in Test).value, bulkyThresholdInLines.value)
-  }
+  //
+  //  override val projectSettings: Seq[Setting[_]] = {
+  //    bulkySources := getBulkyLinesFiles((sources in Compile).value, bulkyThresholdInLines.value)
+  //    (Test / bulkySources) := getBulkyLinesFiles((sources in Test).value, bulkyThresholdInLines.value)
+  //  }
 }
